d9b7a0da1e68b7cefcb76902989cbf774aa9bce8
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-19 17:19:03 +0900

    するかしないか

--- backend/bbb.php
+++ backend/bbb.php
@@ -8,3 +8,19 @@ if(!empty($_POST['your_name'])){
   $your_name = $_GET['your_name'];
   echo $your_name;
 ?>
+
+<?php 
+  //まずフォームが送信されたか確認する必要がある
+  if(isset($_POST['play'])){
+    $play = $_POST['play'];
+    $random_play = rand(0, 1);
+
+    if($random_play === 0){
+      echo '今日はドラゴンクエスト3をプレイしません！明日の為にすやすや寝ます。';
+    }else($random === 1){
+      echo "今日は思う存分ドラクエをプレイしましょうね！＾＾"
+      }
+      endif ?>
+    
+    
+    
