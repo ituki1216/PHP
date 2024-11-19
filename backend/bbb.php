0821cc27af4fceb564bc496b04abcbb511c7fd11
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-19 15:46:42 +0900

    Update bbb.php

--- backend/bbb.php
+++ backend/bbb.php
@@ -1,6 +1,9 @@
 <?php
 if(!empty($_GET['character'])){ // $characterの中には連想配列が格納される
-$character = $_GET['character'];
-echo $character;
+  $character = $_GET['character'];
+  echo $character;
 }
+
+if(!empty($_POST['your_name'])){
+  
 ?>
